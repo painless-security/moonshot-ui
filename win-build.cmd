1a9789cfd07222ad0fd45b6773b72bf8fb067492
Kevin Wasserman <krwasserman@hotmail.com>
2015-02-05 10:07:12 -0500

    update windows build scripts

--- win-build.cmd
+++ win-build.cmd
@@ -1,6 +1,3 @@
 git clean -xdf
-set PATH=c:\mingw\msys\1.0\local\bin;c:\mingw\msys\1.0\bin;c:\mingw\bin;%PATH%
-rem autoreconf -f -i
-rem ./configure
-rem make
-rem make install?
+call windows\win-gnu-env.cmd
+bash win-build
---
+++ windows/win-gnu-env.cmd
@@ -0,0 +1,1 @@
+set PATH=c:\mingw\msys\1.0\local\bin;c:\mingw\msys\1.0\bin;c:\mingw\bin;%PATH%
